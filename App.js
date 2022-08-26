ec4883200fd1726d68fd0c39d1935ae1c09515c2
Muhammad Umair Ahmad <m.umairahmad1@gmail.com>
2022-08-26 23:11:19 +0500

    react navigation - native stack  integrated and folder structure defined

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
-import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import AppNavigator from './src/navigation/AppNavigator'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>App</Text>
-    </View>
+    <AppNavigator />
   )
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    backgroundColor: "white",
-    justifyContent: "center",
-    alignItems: "center"
-  }
-})
---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Screens Frontend
+import Home from "../screens/Frontend/Home"
+import About from "../screens/Frontend/About"
+import Contact from "../screens/Frontend/Contact"
+
+// Screens Auth
+import Login from "../screens/Auth/Login"
+import Register from "../screens/Auth/Register"
+import Forgot from "../screens/Auth/Forgot"
+
+const Stack = createNativeStackNavigator()
+
+export default function AppNavigator() {
+
+    const isAuthenticated = false
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                {isAuthenticated
+                    ? <Stack.Group>
+                        <Stack.Screen name="Home" component={Home} />
+                        <Stack.Screen name="About" component={About} />
+                        <Stack.Screen name="Contact" component={Contact} />
+                    </Stack.Group>
+                    : <Stack.Group
+                        screenOptions={{
+                            headerShown: false,
+                        }}
+                    >
+                        <Stack.Screen name="Login" component={Login} />
+                        <Stack.Screen name="Register" component={Register} />
+                        <Stack.Screen name="Forgot" component={Forgot} />
+                    </Stack.Group>
+                }
+
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
